441ffff8db02ba9c01bcdcbb24b7c5aba58e2d3a
ALIRAZA17 <ialidar2001@gmail.com>
2023-09-12 23:56:01 +0500

    Added dropdowns

---
+++ lib/common/widgets/textFields/app_dropDown_text_field.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppDropdownTextField extends StatefulWidget {
+  const AppDropdownTextField({super.key});
+
+  @override
+  State<AppDropdownTextField> createState() => _AppDropdownTextFieldState();
+}
+
+class _AppDropdownTextFieldState extends State<AppDropdownTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
