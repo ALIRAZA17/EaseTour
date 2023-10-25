2bd6d05d2b68bd13600aba60808da2eaf10a1165
ALIRAZA17 <ialidar2001@gmail.com>
2023-10-25 23:54:54 +0500

    added chat screen

--- lib/screens/chat_screen/ui_chat_screen.dart
+++ lib/screens/chat_screen/ui_chat_screen.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+class ChatScreen extends StatelessWidget {
+  AppBar buildAppBar() => AppBar(
+      elevation: 0,
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.transparent,
+      title: const Row(
+        children: [
+          Column(
+            children: [
+              Text(
+                "Ali Raza",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text("Online"),
+            ],
+          ),
+        ],
+      ));
+
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: const Center(
+        child: Text("Centered Text"),
+      ),
+    );
+  }
+}
